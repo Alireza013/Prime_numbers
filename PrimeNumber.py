57448b932a27ac554cd7f76f135c20685f2fd30c
Alireza <85294640+Alireza013@users.noreply.github.com>
2021-11-20 13:32:11 +0330

    Add files via upload

---
+++ PrimeNumber.py
@@ -0,0 +1,11 @@
+#write a prime numbers from 2 to n
+
+number = int(input("Please enter your number:")) #give number from user
+
+for item in range(2 , number + 1): #consider main number for this loop too
+    for j in range(2 , item):
+        if (item % j) == 0:
+            break
+    else:
+        print(item)
+#codded by Alireza Rahimi
