7273ff4fd8cf1b28c33aaa737c01fe539dbdbb43
Alireza <85294640+Alireza013@users.noreply.github.com>
2021-12-14 13:17:42 +0330

    Main Code

---
+++ PrimeNumber.py
@@ -0,0 +1,12 @@
+# write a prime numbers from 2 to n
+# using python x3
+
+number = int(input("Please enter your number:")) # give number from user
+
+for item in range(2 , number + 1): # consider main number for this loop too
+    for j in range(2 , item): # made counter
+        if (item % j) == 0: # if the answer for dividing item will be zero, don't consider that number
+            break # break and exit the loop
+    else:
+        print(item) # give us answer
+#codded by ALIREZA RAHIMI
